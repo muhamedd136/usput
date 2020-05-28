29d53ded370f6873087b1e79b6626fcb16966170
aldin <aldinberisa1514@gmail.com>
2020-05-28 20:16:19 +0200

    Add new default avatar

--- client/src/components/Avatar/Avatar.js
+++ client/src/components/Avatar/Avatar.js
@@ -1,20 +1,13 @@
 import React from "react";
-
+import person_avatar from "../../assets/person.png";
 import "./Avatar.scss";
 
 const Avatar = (props) => {
-  return (
-    <div className="avatar">
-      <img
-        src={
-          props.avatar
-            ? props.avatar
-            : "https://lh3.googleusercontent.com/proxy/CRoLGshXRUTaru27uJkfutTmMtRGGuVzf87C3bVkdgo0xPvDb1nI3aLCnIRjaW_8nGr58EMerVy0Fny36zLT82a1SmfI4PH8IbWe8oFafrKjsIU9RIdC-GXtvLelI1bHkA"
-        }
-        alt=""
-      />
-    </div>
-  );
+	return (
+		<div className="avatar">
+			<img src={props.avatar ? props.avatar : person_avatar} alt="" />
+		</div>
+	);
 };
 
 export default Avatar;
